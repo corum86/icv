f7da5be84055e65ec228ef2bc65784ac88c79fbb
Sergkei <ser.corum@gmail.com>
2026-09-23 23:25:52 +0200

    refactor: remove unused components and related files

--- frontend/src/components/svg/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ProjectConnector } from './ProjectConnector.vue'
-export { default as SkillConstellation } from './SkillConstellation.vue'
-export { default as TimelinePath } from './TimelinePath.vue'
